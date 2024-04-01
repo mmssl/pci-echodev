92c8d64c656f769c83d4c57631a38fe2a470b754
mmssl <muharremssl.001@gmail.com>
2024-04-01 16:12:58 +0300

    memory mapping updates

--- echodev-drv.c
+++ echodev-drv.c
@@ -1,11 +1,38 @@
 #include <linux/delay.h>
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/pci_regs.h>
+#include <linux/uaccess.h>
+
+#define DEVNR 64
+#define DEVNRNAME "echodev"
 
 #define VID 0x1234
 #define DID 0xbeef
+
+struct echodev {
+  struct pci_dev *pdev;
+} mydev;
+
+static int echo_mmap(struct file *file, struct vm_area_struct *vma) {
+
+  int status;
+  vma->vm_pgoff = pci_resource_start(mydev.pdev, 1) >> PAGE_SHIFT;
+
+  status = io_remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
+                              vma->vm_end - vma->vm_start, vma->vm_page_prot);
+  if (status) {
+    printk("echodev-drv - Error allocating device number\n ");
+    return -status;
+  }
+  return 0;
+}
+
+static struct file_operations fops = {
+    .mmap = echo_mmap,
+};
 
 static struct pci_device_id echo_ids[] = {
 
@@ -19,23 +46,31 @@ MODULE_DEVICE_TABLE(pci, echo_ids);
 static int echo_probe(struct pci_dev *pdev, const struct pci_device_id *id) {
   int status;
   void __iomem *ptr_bar0, __iomem *ptr_bar1;
+  mydev.pdev = pdev;
+  status = register_chrdev(DEVNR, DEVNRNAME, &fops);
+  if (status < 0) {
+    printk("echodev-drv - Error allocating device number\n ");
+    goto fdev;
+  }
   status = pcim_enable_device(pdev);
   if (status != 0) {
 
     printk("echodev-drv - Error enabling device\n ");
-    return status;
+    goto fdev;
   }
 
   ptr_bar0 = pcim_iomap(pdev, 0, pci_resource_len(pdev, 0));
   if (!ptr_bar0) {
     printk("echodev-drv - Error mapping bar0\n");
     return -ENODEV;
+    goto fdev;
   }
 
   ptr_bar1 = pcim_iomap(pdev, 1, pci_resource_len(pdev, 1));
   if (!ptr_bar0) {
     printk("echodev-drv - Error mapping bar1\n");
     return -ENODEV;
+    goto fdev;
   }
   printk("echodev-drv - ID: 0x%x\n", ioread32(ptr_bar0));
   printk("echodev-drv - Random value: 0x%x\n", ioread32(ptr_bar0 + 0xc));
@@ -49,9 +84,14 @@ static int echo_probe(struct pci_dev *pdev, const struct pci_device_id *id) {
   printk("echodev-drv - BAR1 Offset 0: 0x%x\n", ioread16(ptr_bar1));
   printk("echodev-drv - BAR1 Offset 0: 0x%x\n", ioread32(ptr_bar1));
   return 0;
+
+fdev:
+  unregister_chrdev(DEVNR, DEVNRNAME);
+  return status;
 }
 static void echo_remove(struct pci_dev *pdev) {
   printk("Echo dev : Removing the devices\n");
+  unregister_chrdev(DEVNR, DEVNRNAME);
 }
 
 static struct pci_driver echo_driver = {
